6cf7c76065cb4990af86e6978c9b441617cef31a
kaiser-hoque2 <isdbstudent@gmail.com>
2023-11-27 01:52:32 +0600

    medicine table migrate

---
+++ database/migrations/2023_11_26_194726_create_medicines_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('medicines', function (Blueprint $table) {
+            $table->id();
+            $table->id();
+            $table->string('name');
+            $table->string('contact_num');
+
+            $table->string('email');
+            $table->string('address');
+            $table->integer('status');
+
+            $table->string('division');
+            $table->text('description');
+            $table->timestamps();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('medicines');
+    }
+};
