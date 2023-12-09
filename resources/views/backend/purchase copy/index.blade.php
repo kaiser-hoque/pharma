3106dba7c894ec9a343ffcb5f2040414225f5887
mojahidjouel <mojahidjouel95@gmail.com>
2023-12-09 13:51:27 +0600

    tiny edit

---
+++ resources/views/backend/purchase copy/index.blade.php
@@ -0,0 +1,126 @@
+@extends('backend.layouts.app')
+@section('title', trans('create Users'))
+@section('content')
+    <div class="wrapper">
+        <div class="page-wrapper">
+            <div class="page-content">
+                <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
+                    <div class="breadcrumb-title pe-3">Tables</div>
+                    <div class="ps-3">
+                        <nav aria-label="breadcrumb">
+                            <ol class="breadcrumb mb-0 p-0">
+                                <li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-home-alt"></i></a>
+                                </li>
+                                <li class="breadcrumb-item active" aria-current="page">Purchase Data Table</li>
+                            </ol>
+                        </nav>
+                    </div>
+                    <div class="ms-auto">
+                        <div class="btn-group">
+                            <div class="ms-auto"><a href="{{ route('purchase.create') }}"
+                                    class="btn btn-primary radius-30 mt-2 mt-lg-0"><i class="bx bxs-plus-square"></i>Add New
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <h6 class="mb-0 text-uppercase">Purchase</h6>
+                <hr />
+                <div class="card">
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table id="example" class="table table-striped table-bordered" style="width:100%">
+                                <thead>
+                                    <tr>
+                                        <th>{{ __('SL') }}</th>
+                                        <th class="text-center">{{ __('Supplier') }}<br>{{ __('Name') }}</th>
+                                        <th class="text-center">{{ __('Purchase') }}<br>{{ __('date') }}</th>
+                                        <th class="text-center">{{ __('Total') }}<br>{{ __('Quantity') }}</th>
+                                        <th class="text-center">{{ __('Sub') }}<br>{{ __('Amount') }}</th>
+                                        <th class="text-center">{{ __('Grand') }}<br>{{ __('Total') }}</th>
+                                        <th class="text-center">{{ __('Reference') }}<br>{{ __('No') }}</th>
+                                        <th class="text-center">{{ __('Discount') }}</th>
+                                        <th class="text-center">{{ __('Other') }}<br>{{ __('Charge') }}</th>
+                                        <th class="text-center">{{ __('Round') }}<br>{{ __(' Of') }}</th>
+                                        <th class="text-center">{{ __('payment') }}<br>{{ __(' status') }}</th>
+                                        <th class="text-center">{{ __('status') }}
+                                        <th>{{ __('Tax') }}</th>
+
+                                        <th>{{ __('note') }}</th>
+                                        <th class="white-space-nowrap">{{ __('Action') }}</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @forelse($purchase as $value)
+                                    <tr>
+                                        <td>{{++$loop->index}}</td>
+                                        <td>{{$value->supplier?->name}}</td>
+                                        <td>{{$value->purchase_date}}</td>
+                                        <td>{{$value->total_quantity}}</td>
+                                        <td>{{$value->sub_amount}}</td>
+                                        <td>{{$value->grand_total}}</td>
+                                        <td>{{$value->reference_no}}</td>
+                                        <td>{{$value->discount}}</td>
+                                        <td>{{$value->other_charge}}</td>
+                                        <td>{{$value->round_of}}</td>
+                                        <td>{{$value->payment_status}}</td>
+                                        <td>{{($value->status)==1 ? 'active':'inactive'}}</td>
+                                        <td>{{$value->tax}}</td>
+                                      
+                                        <td>{{$value->note}}</td>
+
+
+                                        <td class="action-buttons">
+                                            <div class="button-container">
+                                                <a href="{{route('medicine.edit',encryptor('encrypt', $value->id))}}">
+                                                    <i class="fa fa-edit"></i>
+                                                </a>
+                                                <a href="{{route('medicine.show',encryptor('encrypt',$value->id))}}"><i class="fa-solid fa-eye"></i></a>
+                                                <form id="" action="{{ route('medicine.destroy', $value->id)}}" method="post">
+                                                    @csrf
+                                                    @method('delete')
+                                                    <button style="background: none; border: none;" type="submit">
+                                                        <i class="fa fa-trash text-danger"></i>
+                                                    </button>
+                                                </form>
+
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    @empty
+                                    <tr>
+                                        <th colspan="12" class="text-center">No Product Found</th>
+                                    </tr>
+                                    @endforelse
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            {{-- ================ --}}
+            <style>
+                .action-buttons {
+                    width: 1%;
+                    white-space: nowrap;
+                }
+
+                .button-container {
+                    display: flex;
+                    gap: 10px;
+                }
+            </style>
+
+        </div>
+        <div class="overlay toggle-icon"></div>
+        <a href="javaScript:;" class="back-to-top"><i class='bx bxs-up-arrow-alt'></i></a>
+        <footer class="page-footer">
+            <p class="mb-0">Copyright © 2023. All right reserved.</p>
+        </footer>
+    </div>
+
+    </div>
+
+@endsection
